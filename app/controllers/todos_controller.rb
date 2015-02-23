4169cc183f0b10f025bdc0f93ed6922098c80fe4
Raghu Betina <rbetina@github.com>
2015-02-23 11:02:24 -0600

    Adds new todo form to index page

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -28,7 +28,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.html { redirect_to :back, notice: 'Todo was successfully created.' }
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new }
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,8 @@
 class Todo < ActiveRecord::Base
+  default_scope { order("created_at DESC") }
+
+  scope :incomplete, -> { where(complete: false) }
+  scope :complete, -> { where(complete: true) }
+
   validates :content, presence: true
 end
